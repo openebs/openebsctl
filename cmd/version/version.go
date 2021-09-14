a26e16e7e17ab9246242d007ccfd2568d58dcdf4
Amisha Kumari <amishakumari544@gmail.com>
2021-09-14 18:36:10 +0530

    fixed incorrect display message in version issue #111

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -117,8 +117,12 @@ func checkForLatestVersion(currVersion string) {
 		return
 	}
 
+	currversion := data["spec"].(map[string]interface{})["version"].(string)
 	latestVersion := data["spec"].(map[string]interface{})["version"].(string)
-	if !isLatestVersion(currVersion, latestVersion) {
+	if currversion == "dev" {
+		fmt.Println()
+		fmt.Printf("You are using development version of cli, latest released version is: %s", currversion)
+	} else if !isLatestVersion(currVersion, latestVersion) {
 		// The seperator for the error print
 		fmt.Println()
 		fmt.Printf("You are using an older version (%s) of cli, latest available version is: %s", currVersion, latestVersion)
