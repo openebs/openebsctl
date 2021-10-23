657798d76df7dc50a58759c019690c155b205f4f
Daniel Shuy <daniel_shuy@hotmail.com>
2021-10-23 23:34:52 +0800

    Update generic_test.go

--- pkg/persistentvolumeclaim/generic_test.go
+++ pkg/persistentvolumeclaim/generic_test.go
@@ -24,9 +24,10 @@ import (
 
 func TestDescribeGenericVolumeClaim(t *testing.T) {
 	type args struct {
-		pvc     *corev1.PersistentVolumeClaim
-		pv      *corev1.PersistentVolume
-		casType string
+		pvc       *corev1.PersistentVolumeClaim
+		pv        *corev1.PersistentVolume
+		casType   string
+		mountPods string
 	}
 	tests := []struct {
 		name    string
@@ -36,25 +37,27 @@ func TestDescribeGenericVolumeClaim(t *testing.T) {
 		{
 			name: "All Valid Values",
 			args: args{
-				pv:      &cstorPV1,
-				pvc:     &cstorPVC1,
-				casType: "some-cas",
+				pv:        &cstorPV1,
+				pvc:       &cstorPVC1,
+				casType:   "some-cas",
+				mountPods: "",
 			},
 			wantErr: false,
 		},
 		{
 			name: "PV missing",
 			args: args{
-				pv:      nil,
-				pvc:     &cstorPVC1,
-				casType: "some-cas",
+				pv:        nil,
+				pvc:       &cstorPVC1,
+				casType:   "some-cas",
+				mountPods: "",
 			},
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := DescribeGenericVolumeClaim(tt.args.pvc, tt.args.pv, tt.args.casType); (err != nil) != tt.wantErr {
+			if err := DescribeGenericVolumeClaim(tt.args.pvc, tt.args.pv, tt.args.casType, tt.args.mountPods); (err != nil) != tt.wantErr {
 				t.Errorf("DescribeGenericVolumeClaim() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
