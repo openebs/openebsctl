acc1801e68348aa61792d7092ef1ba699931e497
Harsh Vardhan <harsh.vardhan@mayadata.io>
2021-12-24 15:45:53 +0530

    silence errcheck in benchmark tests
    Signed-off-by: Harsh Vardhan <harsh.vardhan@mayadata.io>

--- pkg/generate/sort_test.go
+++ pkg/generate/sort_test.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2020-2021 The OpenEBS Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package generate
 
 import (
@@ -180,7 +196,7 @@ func BenchmarkSelect(b *testing.B) {
 	for _, bm := range benchmarks {
 		b.Run(bm.name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				bm.args.head.Select(bm.args.size, bm.args.count)
+				_, _, _ = bm.args.head.Select(bm.args.size, bm.args.count)
 			}
 		})
 	}
